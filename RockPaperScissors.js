7010beb775d26771a20be199bec46ba851ec3fb1
Eric Drake <eric.drake2011@gmail.com>
2022-07-11 11:04:00 -0500

    enable game function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,30 +1,76 @@
-// Bank of choices for both players.
-let choiceArr = ["rock", "paper", "scissors"];
-
+//Default messages at end of round
 
 function computerPlay() {
+    let choiceArr = ["rock", "paper", "scissors"];
+
     return choiceArr[Math.floor(Math.random() * (2 - 0 + 1) + 0)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
+    playerSelection = prompt('Rock, Paper, Scissors?');
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(`Computer Choice: ${computerSelection}`);
 
-    return playerSelection === computerSelection ? `Draw, ${playerSelection} and ${computerSelection} are buds.`
-        : playerSelection === "rock" && computerSelection === "scissors" ? `You win! Rock beats Scissors.`
-        : playerSelection === "rock" && computerSelection === "paper" ? `You lose! Paper beats Rock.`
-        : playerSelection === "paper" && computerSelection === "rock" ? `You win! Paper beats Rock.`
-        : playerSelection === "paper" && computerSelection === "scissors" ? `You lose! Scissors beat Paper.`
-        : playerSelection === "scissors" && computerSelection === "paper" ? `You win! Scissors beats Paper.`
-        : playerSelection === "scissors" && computerSelection === "rock" ? `You lose! Rock beats Scissors.`
-        : "What the hell did you input?";
+    if (playerSelection === "rock") {
+        return computerSelection === "paper" ? loseMessage
+            : computerSelection === "scissors" ? loseMessage
+            : drawMessage;
+    } else if (playerSelection === "paper") {
+        return computerSelection === "scissors" ? loseMessage
+            : computerSelection === "rock" ? winMessage
+            : drawMessage;
+    } else if (playerSelection === "scissors")  {
+        return computerSelection === "rock" ? loseMessage
+            : computerSelection === "paper" ? winMessage
+            : drawMessage;
+    } else {
+        return errorMessage;
+    }
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+let playerSelection
+let computerSelection = computerPlay();
+
+const winMessage = `You win! ${playerSelection} beats ${computerSelection}!`;
+const loseMessage = `You lose! ${computerSelection} beats ${playerSelection}!`;
+const drawMessage = "Draw, no winners."
+const errorMessage = "Error, please choose rock, paper, or scissors. Round wash.";
+
+const gameWin = "Game Over. You Win!";
+const gameLose = "Game Over. You Lose!";
+const gameDraw = "Game Over. Draw!";
 
 function game() {
+    const compScore = 0;
+    const playerScore = 0;
+
     for (let i = 0; i < 5; i ++) {
+        let roundCount = i + 1;
         
+        alert(`ROUND: ${roundCount}, Player: ${playerScore} Computer: ${compScore}`);
+
+        playRound(playerSelection, computerSelection);
+
+        if (playRound() = winMessage) {
+            roundCount += 1;
+            playerScore +=1;
+        } else if (playRound() = loseMessage) {
+            roundCount += 1;
+            compScore += 1;
+        } else if (playRound() = drawMessage) {
+            roundCount += 1;
+        } else if (playRound() = errorMessage) {
+            i -= 1;
+        }
+    }
+
+    if (playerScore > compScore) {
+        alert(gameWin);
+    } else if (playerScore < compScore) {
+        alert(gameLose);
+    } else if (playerScore = compScore) {
+        alert(gameDraw);
     }
 }
