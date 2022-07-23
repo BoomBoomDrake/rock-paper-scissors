664b4f117e440627c7f5a7cf76774f587c0e8eac
eric <eric.drake2011@gmail.com>
2022-07-23 17:55:14 -0500

    Add reset button and function Basic UI Skeleton Complete

--- RockPaperScissors.2.js
+++ RockPaperScissors.2.js
@@ -6,8 +6,8 @@ let playerScore = 0;
 let compScore = 0;
 let roundCount = 1;
 
-let playerSelection = "";
-let computerSelection = "";
+let playerSelection = "WEAPON";
+let computerSelection = "WEAPON";
 
 function gameStart() {
     for (i = 1; i < 6; i++) {
@@ -18,7 +18,7 @@ function playRound(button) {
     let input = button.textContent;
 
     playerSelection = input.toLowerCase();
-    console.log(playerSelection); // confirm selection
+    //console.log(playerSelection); // confirm selection
 
     if (playerSelection != undefined) computerTurn(); //starts computer turn
     checkRoundWinner(); //check who won the round;
@@ -29,86 +29,137 @@ function computerTurn() {
 
     if (number === 1) {
         computerSelection = "rock";
-        console.log(computerSelection) //confirm selection
+        //console.log(computerSelection) //confirm selection
     } else if (number === 2) {
         computerSelection = "paper";
-        console.log(computerSelection);//""
+        //console.log(computerSelection);//""
     } else if (number === 3) {
         computerSelection = "scissors";
-        console.log(computerSelection);//""
+        //console.log(computerSelection);//""
     }
 }
 
 function checkRoundWinner() {
-    switch (compScore !== 5 && playerScore !== 0) {
+    switch (compScore !== 5 && playerScore !== 5) {
         case playerSelection === "rock" && computerSelection === "scissors":
             roundCount++;
             playerScore++;
-            console.log("You win. Rock beats Scissors.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You win. Rock beats Scissors.";
+            
             if (compScore === 5 || playerScore === 5) endGame();
             break;
         
         case playerSelection === "paper" && computerSelection === "rock":
             roundCount++;
             playerScore++;
-            console.log("You win. Paper beats Rock.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You win. Paper beats Rock.";
+            
             if (compScore === 5 || playerScore === 5) endGame();
             break;
 
         case playerSelection === "scissors" && computerSelection === "paper":
             roundCount++;
             playerScore++;
-            console.log("You win. Scissors beats Paper.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You win. Scissors beats Paper."
+            
             if (compScore === 5 || playerScore === 5) endGame();
             break;
 
         case playerSelection === "rock" && computerSelection === "paper":
             roundCount++;
             compScore++;
-            console.log("You lose. Paper beats Rock.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You lose. Paper beats Rock.";
+
             if (compScore === 5 || playerScore === 5) endGame();
             break;
 
         case playerSelection === "paper" && computerSelection === "scissors":
             roundCount++;
             compScore++;
-            console.log("You lose. Scissors beats paper.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You lose. Scissors beats Paper.";
+            
             if (compScore === 5 || playerScore === 5) endGame();
             break;
 
         case playerSelection === "scissors" && computerSelection === "rock":
             roundCount++;
             compScore++;
-            console.log("You lose. Rock beats Scissors.");
+
+            scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+            roundDisp.textContent = `Round: ${roundCount}`;
+            selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+            results.textContent = "You lose. Rock. beats Scissors.";
+            
             if (compScore === 5 || playerScore === 5) endGame();
             break;
 
         default:
             roundCount++;
-            console.log("Round draw.");
+            roundDisp.textContent = `Round: ${roundCount}`;
+            results.textContent = "Round Draw.";
+
             if (compScore === 5 || playerScore === 5) endGame();
     }
 }
 
 function endGame() {
-    let result = "";
 
     if (playerScore === compScore) {
-        result = `${playerScore} : ${compScore} Draw! Game Over`;
+        results.textContent = `Draw! Game Over`;
 
     } else if (playerScore > compScore) {
-        result = `${playerScore} : ${compScore} You win! Game Over`;
+        results.textContent = `You win! Game Over`;
 
     } else if (playerScore < compScore) {
-        result = `${playerScore} : ${compScore} You lose! Game Over`;
+        results.textContent = `You lose! Game Over`;
 
     }
+    
+    body.removeChild(btnRock);
+    body.removeChild(btnPaper);
+    body.removeChild(btnScissors);
 
-    console.log(result);
+    body.appendChild(resetBtn);
+
+}
+
+function resetGame() {
+    body.removeChild(resetBtn);
     
     playerScore = 0;
     compScore = 0;
     roundCount = 1;
+
+    scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+    roundDisp.textContent = `Round: ${roundCount}`;
+    selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+    results.textContent = 'Choose Your Weapon';
+
+    body.appendChild(btnRock);
+    body.appendChild(btnPaper);
+    body.appendChild(btnScissors);
+    
+
 }
 
 // UI buttons
@@ -116,21 +167,44 @@ const body = document.querySelector('body');
 const btnRock = document.createElement('button');
 const btnPaper = document.createElement('button');
 const btnScissors = document.createElement('button');
-let scoreBoard = document.createElement('div');
+
+
+// UI Displays
+let scoreBoard = document.createElement('h1');
+let roundDisp = document.createElement('h2');
+let selDisp = document.createElement('h2');
+let results = document.createElement('h3');
+
+// UI Displays Default Text
+scoreBoard.textContent = `You ${playerScore} : ${compScore} Comp`;
+roundDisp.textContent = `Round: ${roundCount}`;
+selDisp.textContent = `${playerSelection} VS. ${computerSelection}`;
+results.textContent = 'Choose Your Weapon';
+
 
 btnRock.textContent = "Rock";
 btnPaper.textContent = "Paper";
 btnScissors.textContent = "Scissors";
 
 body.appendChild(scoreBoard);
+body.appendChild(roundDisp);
+body.appendChild(selDisp);
+body.appendChild(results);
 body.appendChild(btnRock);
 body.appendChild(btnPaper);
 body.appendChild(btnScissors);
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
+    button.classList.add('weapon');
     button.addEventListener('click', () => {
         //console.log(button.textContent);
         playRound(button); 
-    })
-})
+    });
+});
+
+const resetBtn = document.createElement('button');
+resetBtn.textContent = 'Reset';
+resetBtn.addEventListener('click', () => {
+    resetGame();
+});
